f442649346ba572db9079523724d65441cad1361
dorayo <yomingyo@gmail.com>
2017-03-28 15:20:59 +0800

    宋清正_本地配置

--- WolfKill/WolfKill/Classes/GlobeClass/MSUHeader.h
+++ WolfKill/WolfKill/Classes/GlobeClass/MSUHeader.h
@@ -38,9 +38,9 @@
 
 //Debug调试
 #ifdef DEBUG
-#define SLog(format, ...) printf("class: <%p %s:(%d) > method: %s \n%s\n", self, [[[NSString stringWithUTF8String:__FILE__] lastPathComponent] UTF8String], __LINE__, __PRETTY_FUNCTION__, [[NSString stringWithFormat:(format), ##__VA_ARGS__] UTF8String] )
+#define NSLog(format, ...) printf("class: <%p %s:(%d) > method: %s \n%s\n", self, [[[NSString stringWithUTF8String:__FILE__] lastPathComponent] UTF8String], __LINE__, __PRETTY_FUNCTION__, [[NSString stringWithFormat:(format), ##__VA_ARGS__] UTF8String] )
 #else
-#define SLog(format, ...)
+#define NSLog(format, ...)
 #endif
 
 #endif /* MSUHeader_h */
