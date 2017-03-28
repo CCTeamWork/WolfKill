79d0e6f439ec22577b8e036cabfdfeb11118ae7f
dorayo <yomingyo@gmail.com>
2017-03-28 14:55:16 +0800

    框架增加

--- WolfKill/WolfKill/AppDelegate.m
+++ WolfKill/WolfKill/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "MSUMainController.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,15 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    MSUMainController *msu = [[MSUMainController alloc] init];
+    
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    self.window.rootViewController = msu;
+    
+    [self.window makeKeyAndVisible];
+    self.window.backgroundColor = [UIColor whiteColor];
+    
     return YES;
 }
 
